38a705d110fe89808ab6f19576caa6b65628ede8
DESKTOP-2UV8RBP\Danny <ots1120@gmail.com>
2024-11-04 13:41:13 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title>Vue & Nuxt Master Course</q-toolbar-title>
+        <NuxtLink v-slot="{ navigate }" custom to="/">
+          <q-btn stretch flat label="Home" no-caps @click="navigate" />
+        </NuxtLink>
+        <q-separator dark vertical />
+        <NuxtLink v-slot="{ navigate }" custom to="/about">
+          <q-btn stretch flat label="About" no-caps @click="navigate" />
+        </NuxtLink>
+        <q-separator dark vertical />
+
+        <q-btn stretch flat label="Admin" no-caps @click="moveYoutube" />
+        <q-separator dark vertical />
+        <NuxtLink v-slot="{ navigate }" custom to="/admin">
+          <q-btn stretch flat label="Admin" no-caps @click="navigate" />
+        </NuxtLink>
+      </q-toolbar>
+    </q-header>
+    <q-page-container :style="pageContainerStyle">
+      <slot></slot>
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup lang="ts">
+const pageContainerStyle = computed(() => ({
+  maxWidth: '1080px',
+  margin: '0 auto',
+}));
+const moveYoutube = async () => {
+  await navigateTo('https://youtube.com', {
+    external: true,
+    open: { target: '_blanck' },
+  });
+};
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['nuxt-quasar-ui'],
+  typescript: { typeCheck: true },
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  quasar: {
+    /* */
+  },
+});
